60c229dcf99037cde260a4a9632af3f7a619cc88
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-08-13 09:01:35 +0700

    update code structure

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routes: {
-        '/': (context) => const DetailProdukPage(),
+        '/': (context) =>  const OnboardingPage(),
         '/onboarding-page': (context) => const OnboardingPage(),
         '/detail-produk-page': (context) => const DetailProdukPage(),
       },
